67f0e0216bbe76e7b3ff38877c365f46ef88cdda
Pablo <paul.ladiv123@gmail.com>
2018-03-28 00:00:01 +0100

    add post-get routes for /play/:number

--- app.js
+++ app.js
@@ -19,6 +19,18 @@ app.get("/", function(req,res){
     res.render("index");
 });
 
+app.post("/play/:number", function(req, res){
+   var sessData = req.session;
+   var number = req.params.number;
+   var indexUp = parseInt(number) +1;
+   sessData.pathUp = "/play/" + String(indexUp)
+    sessData.indexNumber = number;
+    res.redirect(`/play/${number}`);
+});
+
+app.get("/play/:number", function(req, res){
+   res.send("hey well done this is /play/0")
+});
 
 app.listen(3000, function(){
     console.log("Express app running on port 3000")
