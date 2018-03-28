08a4b785135ab7c61d0b0bbbcfaac6d95ed481c1
Pablo <paul.ladiv123@gmail.com>
2018-03-28 20:31:25 +0100

    update test description

--- cypress/integration/quizGame_spec.js
+++ cypress/integration/quizGame_spec.js
@@ -1,5 +1,5 @@
-describe('Post Resource', function() {
-  it('Creating a New Post', function() {
+describe('Starting the game', function() {
+  it('clicking on Start Game takes the user to /play/0', function() {
     cy.visit('http://127.0.0.1:3000/')     // 1.
 
     cy.get('form')               // 9.
@@ -9,6 +9,7 @@ describe('Post Resource', function() {
       .click()
       .url()
       .should('include', "play/0")
+  });
 
-  })
+  
 })
