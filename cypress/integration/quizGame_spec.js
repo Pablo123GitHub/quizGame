ab0e4a5ceeb4e38697a387bfaae1fee3ec455611
Pablo <paul.ladiv123@gmail.com>
2018-03-28 20:43:55 +0100

    tests that simulate user actions from / to play/0 and then to answer/0

--- cypress/integration/quizGame_spec.js
+++ cypress/integration/quizGame_spec.js
@@ -1,15 +1,23 @@
-describe('Starting the game', function() {
-  it('clicking on Start Game takes the user to /play/0', function() {
-    cy.visit('http://127.0.0.1:3000/')     // 1.
+describe('From clicking on the home page to the answer submission', function() {
+  it('takes the user from / to play/0 and then to answer/0', function() {
+    cy.visit('http://127.0.0.1:3000/')
 
-    cy.get('form')               // 9.
+    cy.get('form')
       .should('contain', 'Start Game')
 
     cy.get('input')
       .click()
       .url()
       .should('include', "play/0")
+
+      cy.get('input.answer_submission')
+        .type('something')
+
+        cy.get('input.submit')
+          .click()
+
+        cy.url()
+            .should('include', 'answer/0')
   });
 
-  
 })
