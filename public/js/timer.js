bc88d0835bb76bd55877d0feeb6a3ae8d4166080
Pablo <paul.ladiv123@gmail.com>
2018-04-01 19:34:11 +0100

    add timer to the page via timer.js in question.ejs view page

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,13 +1,34 @@
 (function(exports){
   exports.Timer = function(){
-    var timerDivLocation = document.getElementById("form_answer");
 
-buildTimer = function(display){
+buildTimer = function(timeInterval){
 
-var timerDivElement = document.createElement("div");
-timerDivElement.className = "timer_class";
-timerDivElement.innerHTML = display;
-timerDivLocation.appendChild(timerDivElement);
+// // Set the date we're counting down to
+var countDownDate = new Date().getTime() + timeInterval;
+
+// // Update the count down every 1 second
+ var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="timer_div_bis"
+  document.getElementById("timer_div_bis").innerHTML =  hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("timer_div_bis").innerHTML = "EXPIRED";
+  }
+}, 1000);
 }
 
 return {
