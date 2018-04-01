aa61078d64a62f66408339aa45180ce22fbaeb96
Pablo <paul.ladiv123@gmail.com>
2018-04-01 18:19:58 +0100

    remove several functions in Clock, and add getIntervalMilSec

--- spec/UnitTest/Clock-spec.js
+++ spec/UnitTest/Clock-spec.js
@@ -37,40 +37,10 @@ jasmine.clock().install();
       expect(subject.getTimeStampFuture()).toEqual(expectedResult.getTime());
   });
 
-describe("Build the timer display with hours/minutes/seconds", function() {
-  var dateOne;
-  var dateTwo;
-  var interval;
-  beforeEach(function(){
-     dateOne = new Date("Sep 6, 2018 15:37:25");
-     dateTwo = new Date("Sep 7, 2018 18:40:32");
-    jasmine.clock().mockDate(dateOne);
-    jasmine.clock().mockDate(dateTwo);
-     interval = dateTwo.getTime() - dateOne.getTime() ;
+  it("shows the countDown interval in milliseconds ", function(){
 
-  })
+    expect(subject.getIntervalMilSec()).toEqual(10000);
 
-  it("outputs '0h 0m 2s' format for the timer", function(){
-    expect(subject.formatTime(0,0,2)).toEqual("0h 0m 2s");
   });
-
-  it("extracts hours from time expressed in milliseconds", function(){
-  expect(subject.extractHourFromMilSec(interval)).toEqual(3)
-});
-
-it("extracts minutes from time expressed in millisec", function(){
-  expect(subject.extractMinFromMilSec(interval)).toEqual(3);
-});
-
-it("extracts seconds from time expressed in millisec", function(){
-    expect(subject.extractSecFromMilSec(interval)).toEqual(7);
-  });
-
-it("outputs '0h 0m 2s' with values extracted from milliseconds timeStamp", function(){
-  expect(subject.displayMilSecNiceFormat(interval)).toEqual("3h 3m 7s");
-});
-
-
-});
 
 });
